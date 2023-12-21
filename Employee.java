b923bede7b7103d28d10d2a2828273b34b47e3ac
RuchikaJamode <ruchikasjamode@gmail.com>
2023-12-21 21:52:33 +0530

    Welcome to line comparision computational programs

---
+++ Employee.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.commandline;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to line comparision computational program");
+	}
+
+}
