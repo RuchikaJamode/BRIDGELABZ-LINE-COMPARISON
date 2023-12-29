daf60797598fd0e95ba7d758e7dffc2b9a204986
RuchikaJamode <ruchikasjamode@gmail.com>
2023-12-29 12:55:39 +0530

    check comparision of three lines

--- Employee.java
+++ Employee.java
@@ -15,16 +15,39 @@ public class Employee<Point> {
 
         System.out.println("Length of the line: " + length);
 	
-        //UC2 is denoting equality of two lines
+        //UC2 is denoting equality of two lines based on the end points
 	
-        line line1Line = new line(0, 0, 3, 4);
-        line line2 = new line(0, 0, 3, 4);
+        line line1Line = new line(1, 0, 3, 4);
+        line line2 = new line(0, 2, 3, 4);
         line line3 = new line(1, 1, 4, 5);
 
         System.out.println("Line 1 equals Line 2: " + line1Line.equals(line2)); // Should be true
         System.out.println("Line 1 equals Line 3: " + line1Line.equals(line3)); 
-	}
 	
+	 line line4 = new line(0, 6, 3, 4);
+     line line5 = new line(7, 0, 3, 4);
+     line line6 = new line(1, 4, 4, 5);
+
+     int comparison1_2 = line4.compareTo(line5);
+     int comparison1_3 = line4.compareTo(line6);{
+
+     if (comparison1_2 == 0) {
+         System.out.println("Line 4 is equal to Line 5");
+     } else if (comparison1_2 < 0) {
+         System.out.println("Line 4 is shorter than Line 5");
+     } else {
+         System.out.println("Line 4 is longer than Line 5");
+     }
+
+     if (comparison1_3 == 0) {
+         System.out.println("Line 4 is equal to Line 6");
+     } else if (comparison1_3 < 0) {
+         System.out.println("Line 4 is shorter than Line 6");
+     } else {
+         System.out.println("Line 4 is longer than Line 6");
+     }
 	
 
+}
+}
 }
--- line.java
+++ line.java
@@ -26,10 +26,25 @@ public class line{
 public boolean equals (line otherline) {
 	  return this.x1 == otherline.x1 && this.y1 == otherline.y1 &&
               this.x2 == otherline.x2 && this.y2 == otherline.y2;
+	  
+	  
 		
 	}
 
+public int compareTo(line otherline) {
+    double thisLength = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    double otherLength = Math.sqrt(Math.pow(otherline.x2 - otherline.x1, 2) + Math.pow(otherline.y2 - otherline.y1, 2));
 
+    if (thisLength < otherLength) {
+        return -1; // This line is shorter
+    } else if (thisLength > otherLength) {
+        return 1; // This line is longer
+    } else {
+        return 0; // Lines are equal in length
+    }
+
+
+}
 }
 
 
