3cb0b31dbe68fdcce17a9ab891bd4e6d3c7033d7
RuchikaJamode <ruchikasjamode@gmail.com>
2023-12-29 12:31:21 +0530

    check eqality of two lines based on the end points

--- Employee.java
+++ Employee.java
@@ -1,12 +1,12 @@
 package com.bridgelabz.commandline;
 import java.lang.Math;
 
-public class Employee {
+public class Employee<Point> {
 
 	public static void main(String[] args) {
 		System.out.println("welcome to line comparision computational program");
 	
-	//uc1
+	//UC1 is denoting 
 	    double x1 = 6.0, y1 = 4.0;
         double x2 = 7.0, y2 = 5.0;
 
@@ -15,6 +15,14 @@ public class Employee {
 
         System.out.println("Length of the line: " + length);
 	
+        //UC2 is denoting equality of two lines
+	
+        line line1Line = new line(0, 0, 3, 4);
+        line line2 = new line(0, 0, 3, 4);
+        line line3 = new line(1, 1, 4, 5);
+
+        System.out.println("Line 1 equals Line 2: " + line1Line.equals(line2)); // Should be true
+        System.out.println("Line 1 equals Line 3: " + line1Line.equals(line3)); 
 	}
 	
 	
--- line.java
+++ line.java
@@ -1,23 +1,36 @@
 package com.bridgelabz.commandline;
 import java.lang.Math;
-public class line {
+public class line{
 
 	private double x1, y1, x2, y2;
+	
 
-    public line(double x1, double y1, double x2, double y2) {
+    public line(double x1, double y1, double x2, double y2){
 		// TODO Auto-generated constructor stub
+    	
 	}
 
-	public void Line(double x1, double y1, double x2, double y2) {
+	
+
+	public void line(double x1, double y1, double x2, double y2) {
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
     }
+	
 
     public double calculateLength() {
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
+public boolean equals (line otherline) {
+	  return this.x1 == otherline.x1 && this.y1 == otherline.y1 &&
+              this.x2 == otherline.x2 && this.y2 == otherline.y2;
+		
+	}
+
+
 }
+
 
 
